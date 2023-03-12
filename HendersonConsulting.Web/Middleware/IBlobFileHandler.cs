b78cce6d5615bd9030e2d39ace53cc178ba7d931
Michael Henderson <mike@mohenderson.com>
2023-03-12 12:50:40 -0400

    Remove old code for restart

--- HendersonConsulting.Web/Middleware/IBlobFileHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HendersonConsulting.Web.Middleware
-{
-    /// <summary>
-    /// This provides interfaces to the <see cref="BlobFileHandler"/> class.
-    /// </summary>
-    public interface IBlobFileHandler
-    {
-    }
-}
--- HendersonConsulting.Web/Scripts/Utilities/climate-weather.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-});
-//# sourceMappingURL=climate-weather.js.map
