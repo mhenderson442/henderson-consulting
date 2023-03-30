a879201b27690a0af2a384b777a924682cf00dbd
Michael Henderson <mike@mohenderson.com>
2023-03-30 16:07:05 -0500

    Added items related to SSL processing

---
+++ pipelines/download-az-acme.ps1
@@ -0,0 +1,13 @@
+param (
+    $DefaultWorkingDirectory
+)
+
+$azAcmeUserUri = "https://github.com/az-acme/az-acme-cli/releases/download/v0.3/cli-v0.3-win-x64.zip"
+$downloadedFile = "${DefaultWorkingDirectory}/cli-v0.3-win-x64.zip"
+
+Write-Host "Get az-acme"
+Invoke-WebRequest -Uri $azAcmeUserUri -OutFile $downloadedFile
+
+
+Write-Host "Unzip file"
+Expand-Archive -Path $downloadedFile $DefaultWorkingDirectory
