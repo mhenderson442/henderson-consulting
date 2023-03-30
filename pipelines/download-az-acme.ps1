f041ac37b17638aea93c0effeda4d2b8d883ec47
Michael Henderson <mike@mohenderson.com>
2023-03-30 16:25:22 -0500

    script edits

--- pipelines/download-az-acme.ps1
+++ pipelines/download-az-acme.ps1
@@ -1,13 +1,12 @@
 param (
+    $downloadedFile,
     $defaultWorkingDirectory
 )
 
 $azAcmeUserUri = "https://github.com/az-acme/az-acme-cli/releases/download/v0.3/cli-v0.3-win-x64.zip"
-$downloadedFile = "${defaultWorkingDirectory}/cli-v0.3-win-x64.zip"
 
 Write-Host "Get az-acme"
 Invoke-WebRequest -Uri $azAcmeUserUri -OutFile $downloadedFile
-
 
 Write-Host "Unzip file"
 Expand-Archive -Path $downloadedFile $defaultWorkingDirectory
