7dc7783e5c6a6d84fe27555fb8a83a5d11c88495
Michael Henderson <mike@mohenderson.com>
2023-04-29 07:51:22 -0400

    Small debug edit

--- hendersonconsultingweb/Program.cs
+++ hendersonconsultingweb/Program.cs
@@ -1,24 +1,49 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// builder.Services.AddHsts(options =>
+// {
+//     options.Preload = true;
+//     options.IncludeSubDomains = true;
+//     options.MaxAge = TimeSpan.FromDays(60);
+//     options.ExcludedHosts.Add("hendersonculsting.dev");
+//     options.ExcludedHosts.Add("www.hendersonculsting.dev");
+// });
+
+// builder.Services.AddHttpsRedirection(options =>
+// {
+//     options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+//     options.HttpsPort = 5001;
+// });
+
+// if (!builder.Environment.IsDevelopment())
+// {
+//     builder.Services.AddHttpsRedirection(options =>
+//     {
+//         options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+//         options.HttpsPort = 443;
+//     });
+// }
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapRazorPages();
 
